f8c6f52976abf2e46d382e0d7c3a79a3216f55ce
grozdan bisevac <113186309+gocegoce@users.noreply.github.com>
2022-09-11 13:43:41 +0200

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ *  PODEŠAVANJE 
+ */
+
+// adresa e-pošte koja će se nalaziti u polju kad stigne vlasniku sajta.
+$from = 'Poruka sa PORTFOLIA <goshabg2@gmail.com>';
+
+// adresa e-pošte koja će primiti e-poštu sa izlazom obrasca
+$sendTo = 'Demo contact form <goshabg1@gmail.com>';
+
+// subject of the email
+$subject = 'New message from contact form';
+
+// form field names and their translations.
+// array variable name => Text to appear in the email
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
+
+// message that will be displayed when everything is OK :)
+$okMessage = 'Tekst koji ste uneli uspešno je poslat. Hvala Vam, brzo ću se javiti!';
+
+// Ako nešto pođe po zlu, prikazaćemo ovu poruku
+$errorMessage = 'Došlo je do greške prilikom slanja poruke. Molimo Vas pokušajte kasnije';
+
+/*
+ *  LET'S DO THE SENDING
+ */
+
+// if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
+error_reporting(E_ALL & ~E_NOTICE);
+
+try
+{
+
+    if(count($_POST) == 0) throw new \Exception('Form is empty');
+            
+    $emailText = "Imate novu poruku\n=============================\n";
+
+    foreach ($_POST as $key => $value) {
+        // If the field exists in the $fields array, include it in the email 
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
+
+    // All the neccessary headers for the email.
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $from,
+        'Return-Path: ' . $from,
+    );
+    
+    // Send email
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+
+    $responseArray = array('type' => 'success', 'message' => $okMessage);
+}
+catch (\Exception $e)
+{
+    $responseArray = array('type' => 'danger', 'message' => $errorMessage);
+}
+
+
+// if requested by AJAX request return JSON response
+if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
+    $encoded = json_encode($responseArray);
+
+    header('Content-Type: application/json');
+
+    echo $encoded;
+}
+// else just display the message
+else {
+    echo $responseArray['message'];
+}
